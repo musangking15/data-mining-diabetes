ead58e3c6d9ec791f06557570036a2bb477f7dc8
musangking15 <haikalfajari156@gmail.com>
2023-11-06 14:13:32 +0700

    fixed bug

--- stream_diabetes.py
+++ stream_diabetes.py
@@ -33,7 +33,7 @@ if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict(
         [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
-    if diab_prediction[0] == 1:
+    if diab_prediction == 1:
         diab_diagnosis = 'Pasien terkena Diabetes'
     else:
         diab_diagnosis = 'Pasien tidak terkena Diabetes'
